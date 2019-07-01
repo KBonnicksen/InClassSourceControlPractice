e277df109066642c266de424f94ce0456f1ff50d
KELSEY Bonnicksen <bonnicksen8017@students.cptc.edu>
2019-07-01 14:13:12 -0700

    Add project files.

---
+++ InClassSourceControlPractice/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InClassSourceControlPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
