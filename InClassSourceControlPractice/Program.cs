d2f0c6ee1c1ea6c38ffa20698fc123f780c77d56
Kelsey Bonnicksen <kelseybon@gmail.com>
2019-07-03 13:45:29 -0700

    Refactored greeting message into separate method

--- InClassSourceControlPractice/Program.cs
+++ InClassSourceControlPractice/Program.cs
@@ -6,37 +6,47 @@ namespace InClassSourceControlPractice
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello there!\nWhat is your favorite primary color?");
+            DisplayGreeting();
 
             String favoriteColor = Console.ReadLine();
+
+            UserColorInformation(favoriteColor);
+
         }
-    }
 
-    private static void UserColorInformation(string color)
-    {
-        color = color.ToLower();
-
-        switch (color)
+        private static void DisplayGreeting()
         {
-            case "red":
-                Console.WriteLine("\nRed paired with the primary color blue makes purple." +
-                    "\nCalifornia's state bird is red." +
-                    "\nThe squiggles underneath code that will not compile is also this color.");
-                break;
-            case "blue":
-                Console.WriteLine("\nBlue paired with yellow creates the color green." +
-                    "\nThe sky is a commonly used example of the color blue." +
-                    "\nThe tears of my enemy are blue.");
-                break;
-            case "yellow":
-                Console.WriteLine("\nYellow paired with the primary color red creates brown." +
-                    "\nYou probably know this, but bananas are yellow." +
-                    "\nSome people think schoolbuses are orange but I think they are yellow.");
-                break;
-            default:
-                Console.WriteLine("\n\n...\n\n\nI don't think that's a primary color.");
-                break;
+            Console.WriteLine("Hello there!\nWhat is your favorite primary color?");
         }
-        Console.ReadKey();
+
+        static void UserColorInformation(string color)
+        {
+            color = color.ToLower();
+
+            switch (color)
+            {
+                case "red":
+                    Console.WriteLine("\nRed paired with the primary color blue makes purple." +
+                        "\nCalifornia's state bird is red." +
+                        "\nThe squiggles underneath code that will not compile is also this color.");
+                    break;
+                case "blue":
+                    Console.WriteLine("\nBlue paired with yellow creates the color green." +
+                        "\nThe sky is a commonly used example of the color blue." +
+                        "\nThe tears of my enemies are blue.");
+                    break;
+                case "yellow":
+                    Console.WriteLine("\nYellow paired with the primary color red creates brown." +
+                        "\nYou probably know this, but bananas are yellow." +
+                        "\nSome people think schoolbuses are orange but I think they are yellow.");
+                    break;
+                default:
+                    Console.WriteLine("\n\n...\n\n\nI don't think that's a primary color.");
+                    break;
+            }
+            Console.ReadKey();
+        }
     }
+
+    
 }
